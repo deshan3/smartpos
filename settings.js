93ea8a82ce9e3cb3a7c2b42dfc714899287ff1d6
deshan3 <58333921+deshan3@users.noreply.github.com>
2025-04-09 21:31:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Professor', // Deshan_K_Rathnamalala
+  ownerNumber: '94726779895', Deshan_K_Rathnamalala//
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
